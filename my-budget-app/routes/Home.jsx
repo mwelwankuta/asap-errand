ef57481516b94a6a799e508756750f53bc94e7c5
Mwelwa Nkuta <mwelwankuta@gmail.com>
2022-04-09 05:48:53 +0200

    first commit

---
+++ my-budget-app/routes/Home.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import React from 'react';
+
+export default function Home() {
+  return (
+    <ScrollView>
+      <View style={{ flex: 1 }}>
+        <Text>Home</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ my-budget-app/routes/Login.jsx
@@ -0,0 +1,14 @@
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function Login() {
+  return (
+    <ScrollView>
+      <View style={{ flex: 1 }}>
+        <Text>Home</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({});
